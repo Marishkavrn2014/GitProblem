21b2639d36ac191284c26fcab53d4e981a4ec9a2
Marina <marishka_vrn@list.ru>
2020-07-22 12:59:55 +0300

    some updates

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        String name = args.length < 1 ? "Default" : args[0];
+        System.out.println("Hello, " + name + "!");
     }
 }
