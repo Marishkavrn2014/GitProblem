e7e8fb8ab72016d61b1b378a3616adc6effaacbb
Marina <marishka_vrn@list.ru>
2020-07-22 13:03:46 +0300

    Revert "some updates"
    This reverts commit 21b2639d36ac191284c26fcab53d4e981a4ec9a2.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        String name = args.length < 1 ? "Default" : args[0];
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, world!");
     }
 }
