eea9110e4cdd57017677d9ad831d682fb880cbaa
Marina <marishka_vrn@list.ru>
2020-07-22 11:15:59 +0300

    resolving the conflicts

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
         String name;
-        int SIZE = args.length;
-        name = SIZE > 1 ? args[0] : "World";
+
+        if(args.length < 1) {
+            name = "World";
+        } else {
+            name = args[0];
+        }
 
         System.out.println("Hello, " + name + "! Have a nice day! This is cherry-pick!");
     }
