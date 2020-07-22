ff250fd53e2d16ad84b9b8c338011226c3364f49
Marina <marishka_vrn@list.ru>
2020-07-22 11:15:59 +0300

    add default name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        String name;
 
         if(args.length < 1) {
-            System.out.println("Please enter your name");
-            System.exit(0);
+            name = "World";
+        } else {
+            name = args[0];
         }
-        String name = args[0];
+
         System.out.println("Hello, " + name + "!");
     }
 }
